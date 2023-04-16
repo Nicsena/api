ed93584ee89cd8789bbc23cd67a0d80de2c081a0
Nicsena <Nicholassena8190@gmail.com>
2023-04-15 22:09:34 -0700

    Add GitHub Route - Not Finished

---
+++ routes/github.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/", (res, req) => {
+    
+});
+
+router.post("/webhook/update", (res, req) => {
+    var body = res.body;
+    var event = body["hook"]["events"][0];
+
+    console.log(event)
+    console.log(res.headers)
+
+    //console.log(res.body);
+    req.status(200).json({ message: "OK" })
+
+});
+
+module.exports = router
