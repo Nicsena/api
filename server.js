f034e1331440d81e2cdf5745796ffeb7be377d4f
Nicsena <Nicholassena8190@gmail.com>
2023-05-17 23:31:36 -0700

    Add version to /

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const os = require('os');
 const humanizeDuration = require("humanize-duration");
 const path = require("path")
+const package = require("./package.json")
 
 const { formatBytes } = require("./src/utility")
 const { MemoryUsage } = require("./src/system")
@@ -67,7 +68,7 @@ mongodb_db.on("error", function (err) {
 // -- PUBLIC ENDPOINTS --
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "OK" });
+  res.status(200).json({ message: "OK", version: package["version"] });
 })
 
 app.get('/ping', (req, res) => {
